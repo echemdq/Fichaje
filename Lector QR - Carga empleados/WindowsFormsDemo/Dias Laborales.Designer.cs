1adf135cca3857e040ee0435ce76e77ad6577c2b
Ezequiel Vazquez <eche.mdq@hotmail.com>
2015-02-25 20:22:07 -0300

    botton nuevo

--- Lector QR - Carga empleados/WindowsFormsDemo/Dias Laborales.Designer.cs
+++ Lector QR - Carga empleados/WindowsFormsDemo/Dias Laborales.Designer.cs
@@ -51,6 +51,7 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // chk_noct
@@ -307,12 +308,22 @@
             this.maskedTextBox1.ValidatingType = typeof(System.DateTime);
             this.maskedTextBox1.Validated += new System.EventHandler(this.maskedTextBox1_Validated);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(376, 265);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 72;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Diaslaborales
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Info;
             this.ClientSize = new System.Drawing.Size(597, 368);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.maskedTextBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -371,5 +382,6 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Button button1;
     }
 }

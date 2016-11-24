a52e7c05bd89860d0ce12612402787009e37935a
echemdq <eche.mdq@hotmail.com>
2016-11-24 10:18:01 -0300

    correccion ordenamiento combobox
    1

--- Lector QR - Carga empleados/WindowsFormsDemo/WindowsFormsDemoForm.cs
+++ Lector QR - Carga empleados/WindowsFormsDemo/WindowsFormsDemoForm.cs
@@ -648,7 +648,7 @@ namespace WindowsFormsDemo
                     cmb_tiponov.ValueMember = "idtiponovedad";
                     cmb_tiponov.SelectedIndex = 0;
 
-                    cmb_horarios.DataSource = oacceso.leerDatos("select concat(detalle,case when horario=1 then ' Corrido:' else ' Cortado:' end,ingreso1,'-',egreso1,'-',ingreso2,'-',egreso2) as detalle, idhorarios from horarios");
+                    cmb_horarios.DataSource = oacceso.leerDatos("select concat(detalle,case when horario=1 then ' Corrido:' else ' Cortado:' end,ingreso1,'-',egreso1,'-',ingreso2,'-',egreso2) as detalle, idhorarios from horarios order by detalle asc");
                     cmb_horarios.DisplayMember = "detalle";
                     cmb_horarios.ValueMember = "idhorarios";
                     cmb_horarios.SelectedIndex = 0;
